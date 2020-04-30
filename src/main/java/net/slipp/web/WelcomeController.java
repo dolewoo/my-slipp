ec1f7299168a6a7cd8c128667d101f1d9d306aa3
dolewoo <dolewoo@gmail.com>
2020-05-01 01:50:08 +0900

    webapp 설정

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/helloworld")
-	public String Welcome(String name, int age, Model model) {
-		System.out.println("name : "+name);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String Welcome(Model model) {
+		//System.out.println("name : "+ name);
+		model.addAttribute("name", "한길우");
+		model.addAttribute("value", 4000000);
+		model.addAttribute("taxed_value",400000*0.67 );
 		return "welcome";
 	}
 }
